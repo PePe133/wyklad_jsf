50ce5646c6bb763543f0a27ce5539a59f53e5ee3
piotrgrobelnyedukacja <44403770+piotrgrobelnyedukacja@users.noreply.github.com>
2020-11-20 10:06:42 +0100

    usuniecie pliku testowego

--- PORTALE_REST/web/testowanieGita.js
+++
@@ -1,9 +0,0 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-alert( 'Hello, world!' );
-document.write("Ten plik został dodany grupowo");
